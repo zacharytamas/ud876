841f7a40a295f455ffc083fd62b4a89d5b24e453
Zachary Jones <zacharytamas@gmail.com>
2015-08-06 15:58:05 -0400

    Complete our background Activity listening service

---
+++ Location2_2/app/src/main/java/com/example/android/location2_2/Constants.java
@@ -0,0 +1,10 @@
+package com.example.android.location2_2;
+
+/**
+ * Created by zacharytamas on 8/6/15.
+ */
+public class Constants {
+
+    public static String BROADCAST_ACTION = "broadcast_action";
+
+}
--- Location2_2/app/src/main/java/com/example/android/location2_2/DetectedActivitiesIntentService.java
+++ Location2_2/app/src/main/java/com/example/android/location2_2/DetectedActivitiesIntentService.java
@@ -2,19 +2,33 @@ package com.example.android.location2_2;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zacharytamas on 8/6/15.
  */
 public class DetectedActivitiesIntentService extends IntentService {
     protected static final String TAG = "detection_is";
+    public static final String DETECTED_ACTIVITIES_EXTRA = "detected_activities_extra";
 
-    DetectedActivitiesIntentService() {
+    public DetectedActivitiesIntentService() {
         super(TAG);
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        ActivityRecognitionResult recognitionResult = ActivityRecognitionResult.extractResult(intent);
 
+        Intent newIntent = new Intent(Constants.BROADCAST_ACTION);
+
+        ArrayList<DetectedActivity> activities = (ArrayList) recognitionResult.getProbableActivities();
+        newIntent.putExtra(DETECTED_ACTIVITIES_EXTRA, activities);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 }
